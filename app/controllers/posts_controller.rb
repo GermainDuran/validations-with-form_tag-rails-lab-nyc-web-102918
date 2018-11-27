53678d025ab974538e53ef3076a3cb4afbfcdfe0
GermainDuran <arturodurantorres@gmail.com>
2018-11-27 15:07:21 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,29 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def create
+    @post = Post.new(author_params)
+      if @post.save
+        redirect_to post_path(@post)
+      else
+        render :new
+      end
+
+  end
+
+  def edit
+   @post = Post.find(params[:id])
+  end
+
+  def update
+   @post = Post.find(params[:id])
+   if @post.update(post_params)
+     redirect_to post_path(@post)
+   else
+     render :edit
+   end
+ end
+
   private
 
   def post_params
